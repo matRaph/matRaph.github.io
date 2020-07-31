451e7481fcad2a92fa6102e97ceb1f6f6cd88f2c
Raphael Matias <42880393+matRaph@users.noreply.github.com>
2020-07-31 04:02:09 -0300

    Add files via upload

---
+++ GameHtml2/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1596178834,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/back1.png",
+		"images/back0.png",
+		"images/char-sheet0.png",
+		"images/char-sheet1.png",
+		"images/shadow-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
