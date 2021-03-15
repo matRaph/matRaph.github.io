1fed9e47a3fce9e5ae7e11471a64cc6470471f1e
Raphael Matias <42880393+matRaph@users.noreply.github.com>
2021-03-15 13:45:30 -0300

    GameHtml5

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1615826565,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/back1.png",
+		"images/back0.png",
+		"images/char1-sheet0.png",
+		"images/shadowp1-sheet0.png",
+		"images/char2-sheet0.png",
+		"images/char2-sheet1.png",
+		"images/enemy-sheet0.png",
+		"images/enemy-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
